63ebb236a3aec698ca941f0de9d634d31abeb0b0
lijihong <Keikouri802@gmail.com>
2025-10-25 01:39:35 +0900

    菜单样式

--- angular/MyGitHub/src/app/html-component/html-component.component.ts
+++ angular/MyGitHub/src/app/html-component/html-component.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./html-component.component.css']
 })
 export class HtmlComponentComponent implements OnInit {
-
+  disable01:boolean = false;
+  disable02:boolean = false;
+  disable03:boolean = false;
+  disable04:boolean = false;
+  disable05:boolean = false;
+  disable06:boolean = false;
+  disable07:boolean = false;
+  disable08:boolean = false;
+  disable09:boolean = false;
+  disable10:boolean = false;
+  disable11:boolean = false;
+  disable12:boolean = false;
+  disable13:boolean = false;
+  disable14:boolean = false;
+  disable15:boolean = false;
+  disable16:boolean = false;
+  disable17:boolean = false;
+  disable18:boolean = false;
+  disable19:boolean = false;
+  disable20:boolean = false;
+  disable21:boolean = false;
+  disable22:boolean = false;
+  disable23:boolean = false;
+  disable24:boolean = false;
+  disable255:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
