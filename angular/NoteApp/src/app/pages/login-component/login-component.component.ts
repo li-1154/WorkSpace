80c680d38555fe118e5244ecdc5d795844dda8b8
jihong li <Keikouri802@gmail.com>
2025-10-27 08:55:47 +0900

    注册实现

--- angular/NoteApp/src/app/pages/login-component/login-component.component.ts
+++ angular/NoteApp/src/app/pages/login-component/login-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponent implements OnInit {
+  email:string='';
+  password:string= '';
+  errorMsg:string = '';
 
-  constructor() { }
+  constructor(private afs:AngularFireAuth,private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  async login() { 
+     try{
+        await this.afs.signInWithEmailAndPassword(this.email,this.password)
+        console.log('✅ Login success!');
+        this.router.navigate(['/notes']);
+    }
+    catch(error){
+      this.errorMsg = error.message;
+      console.error('❌ Login failed:', error);
+    }
+  }
+  
 }
--- angular/NoteApp/src/app/pages/register-component/register-component.component.ts
+++ angular/NoteApp/src/app/pages/register-component/register-component.component.ts
@@ -8,26 +8,36 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  email:string;
-  password:string;
-  errorMsg:string;
+  errorMessages: { [key: string]: string } = {
+    'auth/email-already-in-use': '该邮箱已被注册，请直接登录。',
+    'auth/invalid-email': '邮箱格式不正确。',
+    'auth/weak-password': '密码太弱,请设置至少6位密码。',
+    'auth/missing-password': '请输入密码。',
+  };
 
-  constructor(private afs:AngularFireAuth,private router:Router ) { }
 
-  async register()
-  {
-    try{
-      await this.afs.createUserWithEmailAndPassword(this.email,this.password)
+
+  email: string;
+  password: string;
+  errorMsg: string;
+
+  constructor(private afs: AngularFireAuth, private router: Router) { }
+
+  async register() {
+    try {
+      await this.afs.createUserWithEmailAndPassword(this.email, this.password)
       console.log('register,success!');
       this.router.navigate(['/login']);
     }
-    catch(error)
-    {
-      this.errorMsg = error.message;
-      console.error('register,failed',error);
+    catch (error) {
+      console.log('❌ Firebase Error:', error);
+      this.errorMsg = error.message; // 临时先显示英文
+      // this.errorMsg = this.errorMessages[error.code]||'注册失败，请稍后再试。';
+      // console.error('register,failed', error);
     }
   }
   ngOnInit(): void {
+    console.log('测试', this.afs);
   }
 
 }
