e87639228d0b8a6b0dcfb26a7510c03e7257d329
jihong li <Keikouri802@gmail.com>
2025-10-31 08:12:23 +0900

    网页发布

--- angular/NoteApp/src/environments/environment.prod.ts
+++ angular/NoteApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+
 export const environment = {
-  production: true
-};
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyA9yxdPAi8r6aB0hDXbaze9aGlUsppAOfc",
+  authDomain: "note-app-a43bc.firebaseapp.com",
+  projectId: "note-app-a43bc",
+  storageBucket: "note-app-a43bc.firebasestorage.app",
+  messagingSenderId: "208128840633",
+  appId: "1:208128840633:web:79007045374b0710051c68"
+}};
